5a77907a997a45ea81b1893efd2b5ff938403ad3
MarasDriver <mkamzel@hotmail.com>
2022-03-14 00:49:45 +0100

    calkulator 2.1

---
+++ Calc2.1.py
@@ -0,0 +1,70 @@
+import time
+t1=time.time()
+
+print("Podaj x")
+x = input()
+x = float(x)
+
+print("(*,/,+,-)\nMnożymy *\nDzielimy /\nDodajemy +\nOdejmujemy - ")
+o=input()
+while o=="":
+    print("Ej, ale tak się nie bawię :)")
+    break
+else:
+    t2=time.time()-t1
+    t2=int(t2)
+    print("WOW, zdecydowałes się w", t2, "sekund(ę/y), koooozak!")
+
+z="Dupa a nie kalkulator"
+
+if o=="*":
+    print("Okej, więc mnożymy x*y")
+    print("Przez co mnożymy?")
+    y=input()
+    y=float(y)
+    z=x*y
+    z=int(z)
+
+if o=="/":
+    print("Okej, więc dzielimy")
+    print("Podaj dzielnik")
+    y = input()
+    y = float(y)
+    while y==0:
+        try:
+            z=x/y
+        except ZeroDivisionError:
+            print("Ty pacanie, co z Ciebie za inżynier")
+            print("Dzielisz przez 0")
+            time.sleep(5)
+            print("Facepalm \n")
+            time.sleep(2)
+            break
+    else:
+        z = x / y
+
+
+
+if o=="+":
+    print("Okej, no to dodajemy")
+    print("Podaj y")
+    y = input()
+    y = float(y)
+    z = x + y
+
+if o=="-":
+    print("Czyli odejmowanie, wporządku")
+    print("Podaj y")
+    y = input()
+    y = float(y)
+    z = x - y
+
+while z=="Dupa":
+    print("Wynik to", z)
+    break
+else:
+    print("Wynik to", z)
+    time.sleep(1)
+    t=time.time()-t1
+    t=int(t)
+    print("Policzyłes to w", t, "sekund(ę/y), brawo!")
