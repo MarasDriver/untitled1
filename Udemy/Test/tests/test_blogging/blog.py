a3ea37bfd40567d7923d30d4f77d88feef658520
MarekK <mkamzelski@adva.com>
2022-12-30 14:34:58 +0100

    blog

--- Udemy/Test/tests/test_blogging/blog.py
+++ Udemy/Test/tests/test_blogging/blog.py
@@ -1,6 +1,3 @@
-from post import Post
-
-
 class Blog:
     def __init__(self, title, author):
         self.title = title
@@ -8,16 +5,15 @@ class Blog:
         self.posts = []
 
     def __repr__(self):
-        return '{} by {} ({} posts)'.format(self.title, self.author, len(self.posts))
+        return '{} by {} ({} post{})'.format(self.title, self.author, len(self.posts), "s" if len(self.posts) != 1 else "")
 
-    def create_post(self, title, content):
-        self.posts.append(Post(title, content))
-
-    def json(self):
-        return {
-            'title': self.title,
-            'author': self.author,
-            'posts': [post.json() for post in self.posts],
-        }
-
+    # def create_post(self, title, content):
+    #     self.posts.append(Post(title, content))
+    #
+    # def json(self):
+    #     return {
+    #         'title': self.title,
+    #         'author': self.author,
+    #         'posts': [post.json() for post in self.posts],
+    #     }
     #
