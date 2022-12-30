b14c8030f0cac52422f77207bb3feca6f3b07f90
MarekK <mkamzelski@adva.com>
2022-12-30 10:36:12 +0100

    test haso

---
+++ Udemy/Test/testing_hasło/test_hasło.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from hasło import defklas
+
+
+class HasloTest(TestCase):
+    def sprawdz_haslo(self):
+        czytaj = open(r"/hasło.txt", "r+")
+        print(czytaj)
+        p = defklas()
+
+        self.assertEqual('dupa', czytaj)
