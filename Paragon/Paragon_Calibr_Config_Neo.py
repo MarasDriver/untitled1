7855124604b03508e11384bb76c7ac63de6cf04c
MarekK <mkamzelski@adva.com>
2024-03-08 12:42:22 +0100

    Kalibracja 20
    Ogarniam kuwete

--- Paragon/Paragon_Calibr_Config_Neo.py
+++ Paragon/Paragon_Calibr_Config_Neo.py
@@ -1,193 +1,114 @@
-import paramiko
+from __future__ import print_function
+import datetime
+import sys
+import math
 import time
+from calnexRest import calnexInit, calnexGet, calnexSet, calnexCreate, calnexDel as p
+# import paragon as p
 import Setup_Conifguration_file
-import linecache
-RxandTx=[]
-
-class telnet_Calibration_file_operation():
-    def open_calibration_file(device,host,port):
-        print(f"Logging into Host: "+host+" on Port:",port)
-        user="root"
-        Password="ChgMeNOW"
-        client= telnetlib.Telnet()
-        client.open(host, port, 5)
-        client.exec_command('\r'.encode('ascii'))
-        client.read_until(b'Login')
-        client.exec_command(user.encode('ascii'))
-        client.read_until(b'root')
-        client.exec_command(b'\r')
-        client.exec_command(b'\x0D')
-        time.sleep(2)
-        client.read_until(b"Password:")
-        client.exec_command('ChgMeNOW'.encode('ascii'))
-        client.exec_command('\r'.encode('ascii'))
-        time.sleep(2)
-        client.exec_command('con sys'.encode('ascii'))
-        client.exec_command('\r'.encode('ascii'))
-        time.sleep(2)
-        client.exec_command('Qdebug Udebug arris0123'.encode('ascii'))
-        client.exec_command('\r'.encode('ascii'))
-        time.sleep(2)
-        client.exec_command('cd ..'.encode('ascii'))
-        client.exec_command('\r'.encode('ascii'))
-        time.sleep(2)
-        client.exec_command('cd etc'.encode('ascii'))
-        client.exec_command('\r'.encode('ascii'))
-        time.sleep(2)
-        client.exec_command(("vim osa"+device+"_calibration.xml").encode('ascii'))
-        client.exec_command('\r'.encode('ascii'))
-        time.sleep(1)
-        i=0
-        while i<90:
-            client.exec_command('\n'.encode('ascii'))
-            i=i+1
-        z_xmla = client.read_until(b"</resources>")
-        z_xmla=str(z_xmla)
-        zapisz = open("xml_kalibracyjny.txt", "w+")
-        zapisz.write(z_xmla)
-        client.exec_command(':qa!'.encode('ascii'))
-        client.exec_command('\r'.encode('ascii'))
-        time.sleep(2)
-        client.exec_command('exit'.encode('ascii'))
-        client.exec_command('\r'.encode('ascii'))
-        time.sleep(2)
-        client.exec_command('logout'.encode('ascii'))
-        client.exec_command('\r'.encode('ascii'))
-        time.sleep(2)
-
-class phy_Delays():
-    def getPhyDelays(self, host=Setup_Conifguration_file.run.host, port=Setup_Conifguration_file.run.port, acc_port=Setup_Conifguration_file.run.acc_port, mediatype=Setup_Conifguration_file.run.mediatype, speed_Phy=Setup_Conifguration_file.run.speed_Phy):
-        print(f"Logging into Host: "+host)
-        user="root"
-        Password="ChgMeNOW"
-        client = paramiko.client.SSHClient()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(host, username=user, password=Password)
-
-        client.exec_command(user.encode('ascii'))
-        client.exec_command(Password.encode('ascii'))
-        client.exec_command('con sys'.encode('ascii'))
-        client.exec_command('Qdebug Udebug arris0123'.encode('ascii'))
-        client.exec_command('devTools-dc'.encode('ascii'))
-        client.exec_command('con hal 2'.encode('ascii'))
-        client.exec_command('fpgadebug'.encode('ascii'))
-        getphy_command = "getPhyDelays " + str(mediatype) + " " + str(speed_Phy) + " " + str(acc_port)
-        client.exec_command((str(getphy_command)).encode('ascii'))
-        z_PhyDelays=z_PhyDelays[135:170]
-        zPHyDelays_rx = z_PhyDelays[:20]
-        zPHyDelays_tx = z_PhyDelays[20:]
-        zPHyDelays_rx=str(zPHyDelays_rx)
-        zPHyDelays_tx=str(zPHyDelays_tx)
-
-        zapisz = open("z_PhyDelays.txt", "w+")
-        z_PhyDelays2=zPHyDelays_rx+"\r"+zPHyDelays_tx
-        # print(z_PhyDelays2)
-        zapisz.write(z_PhyDelays2)
-        client.exec_command('exit'.encode('ascii'))
-
-        client.exec_command('exit'.encode('ascii'))
-
-        client.exec_command('logout'.encode('ascii'))
-        zPHyDelays_rx=zPHyDelays_rx
-        zPHyDelays_tx=zPHyDelays_tx
-        RxandTx=[]
-        RxNumbers = []
-        TxNumbers = []
-        # print(RxandTx)
-        Rx = 0
-        Tx = 0
-        for char in zPHyDelays_rx:
-            # print(char)
-            if char.isdigit():
-                RxNumbers.append(int(char))
-        for char in zPHyDelays_tx:
-            if char.isdigit():
-                TxNumbers.append(int(char))
-        for char in RxNumbers:
-            Rx = Rx * 10 + char
-        RxandTx.append(int(Rx))
-        for char in TxNumbers:
-            Tx = Tx * 10 + char
-        RxandTx.append(int(Tx))
-        zapisz = open("RxandTx.txt", "w+")
-        RxandTx = str(Rx)+"\n"+str(Tx)
-        zapisz.write(RxandTx)
-        print("Rx in XML was equal to:", Rx, ", and Tx in XML was equal to:", Tx)
-        RxandTx = [Rx,Tx]
-        # print(RxandTx)
-        return Rx, Tx,RxandTx
-        client.close()
-        time.sleep(2)
+import Telnet_OSACalibrationXML
 
 
-    def setDelays(self, host=Setup_Conifguration_file.run.host, port=Setup_Conifguration_file.run.port, acc_port=Setup_Conifguration_file.run.acc_port, mediatype=Setup_Conifguration_file.run.mediatype, speed_Phy=Setup_Conifguration_file.run.speed_Phy):
-        print(f"Logging into Host: " + host + " on Port:", port)
-        user = "root"
-        Password = "ChgMeNOW"
-        client = paramiko.client.SSHClient()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(host, username=user, password=Password)
+sys.path.append('//10.12.152.10/calnex100g/RemoteControl/')
 
-        client.exec_command(user.encode('ascii'))
-        client.exec_command(Password.encode('ascii'))
-        client.exec_command('con sys'.encode('ascii'))
-        client.exec_command('Qdebug Udebug arris0123'.encode('ascii'))
-        client.exec_command('devTools-dc'.encode('ascii'))
-        client.exec_command('con hal 2'.encode('ascii'))
-        client.exec_command('fpgadebug'.encode('ascii'))
-        client.exec_command('\r'.encode('ascii'))
-        plik="New_RxandTx.txt"
-        zapisz = open(plik, "a+")
-        newRx = linecache.getline(plik, 1)
-        newTx = linecache.getline(plik, 2)
-        zapisz.close()
-        newRx = newRx.strip("\n")
-        newTx = newTx.strip("\n")
-        newRx = newRx.strip("")
-        newTx = newTx.strip("")
-        newRx = int(newRx)
-        newTx = int(newTx)
-        print("NewRx =", newRx, "NewTx =", newTx)
-        getphy_command = "setPhyDelays " + str(mediatype) + " " + str(speed_Phy) + " " + str(newRx)+ " " + str(newTx)+ " " + str(acc_port)
-        client.exec_command((str(getphy_command)).encode('ascii'))
-        client.exec_command('exit'.encode('ascii'))
-        client.exec_command('exit'.encode('ascii'))
-        client.exec_command('logout'.encode('ascii'))
-        client.close()
-        time.sleep(2)
+# Set instrument IP
+calnexInit("10.12.152.10")
 
 
-    def jump_acc_port(self, host=Setup_Conifguration_file.run.host, port=Setup_Conifguration_file.run.port, acc_port=Setup_Conifguration_file.run.acc_port, mediatype=Setup_Conifguration_file.run.mediatype, speed_Phy=Setup_Conifguration_file.run.speed_Phy):
-        print("Restart Access Port:",acc_port,"Logging into Host: " + host + " on Port:", port)
-        user = "root"
-        Password = "ChgMeNOW"
-        client = paramiko.client.SSHClient()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        # client.connect(host, disabled_algorithms={'pubkeys': ['rsa-sha2-256', 'rsa-sha2-512']})
-        client.connect(host, username=user, password=Password)
+sys.path.insert(0, 'C:/Users/marekk/Documents/Calnex/Paragon-X/RemoteControl/Python/')
+measurement_time = 50
+czas_przed = time.time()
+teraz = datetime.datetime.now()
+teraz = teraz.strftime("%d.%m.%Y %H.%M")
+teraz = str(teraz)
+destination = "D:/Robocze/P-X Automation/Results/"
+
+# ścieżkaPDF = str(destination + teraz + " " + Setup_Conifguration_file.run.paragon_mediaType + " " + Setup_Conifguration_file.run.paragon_speed + r".pdf")
+# ścieżkaCPD = str(destination + teraz + " " + Setup_Conifguration_file.run.paragon_mediaType + " " + Setup_Conifguration_file.run.paragon_speed + r".cpd")
+# print(ścieżkaCPD + "\n" + ścieżkaPDF)
+# paragon Config:
+print(Setup_Conifguration_file.run.paragon_mediaType, Setup_Conifguration_file.run.paragon_speed)
+teraz = datetime.datetime.now()
+print("Running == 1, Paragon Configuration in progress...")
+host = Setup_Conifguration_file.run.host
+port = Setup_Conifguration_file.run.port
+acc_port = Setup_Conifguration_file.run.acc_port
+mediatype = Setup_Conifguration_file.run.mediatype
+paragon_speed = Setup_Conifguration_file.run.paragon_speed
+paragon_mediaType = Setup_Conifguration_file.run.paragon_mediaType
+speed_Phy= Setup_Conifguration_file.run.speed_Phy
+OSA_tested_speed= Setup_Conifguration_file.run.OSA_tested_speed
+OSA_other_speed= Setup_Conifguration_file.run.OSA_other_speed
 
 
-        client.exec_command(user.encode('ascii'))
-        client.exec_command(Password.encode('ascii'))
-        client.exec_command('network-element ne-1'.encode('ascii'))
-        client.exec_command('conf nte nte'.encode('ascii'))
-        acc_port_str=str(acc_port)
-        conf_acc_port=str("conf access-port access-1-1-1-"+acc_port_str)
-        client.exec_command(conf_acc_port.encode('ascii'))
-        client.exec_command('admin-state unassigned'.encode('ascii'))
-        other_speed=str(Setup_Conifguration_file.run.OSA_other_speed)
-        tested_speed = str(Setup_Conifguration_file.run.OSA_tested_speed)
-        speed_to_set=str('speed '+other_speed)
-        client.exec_command(speed_to_set.encode('ascii'))
-        speed_to_set = str('speed '+tested_speed)
-        client.exec_command(speed_to_set.encode('ascii'))
-        client.exec_command('admin-state in-service'.encode('ascii'))
-        client.exec_command('logout'.encode('ascii'))
-        client.close()
-        time.sleep(2)
+class paragon_config():
+    def create_path(self):
+        teraz = datetime.datetime.now()
+        teraz = teraz.strftime("%d.%m.%Y %H.%M")
+        teraz = str(teraz)
+        global ścieżkaCPD, ścieżkaPDF
+        ścieżkaCPD = str(destination + teraz + " " + Setup_Conifguration_file.run.paragon_mediaType + " " + Setup_Conifguration_file.run.paragon_speed + r".cpd")
+        ścieżkaPDF = str(destination + teraz + " " + Setup_Conifguration_file.run.paragon_mediaType + " " + Setup_Conifguration_file.run.paragon_speed + r".pdf")
+        # print(ścieżkaCPD + "\n" + ścieżkaPDF)
+        print(ścieżkaPDF)
+        return ścieżkaCPD, ścieżkaPDF
+
+    def config_calibration(self):
+        p.connect("10.12.152.3", "localhost", 9000, 9990)
+        p.stopcapture()
+        p.paragonset("SimulMeasImpairMode", "MEASUREONLY")
+        p.paragonset("OperatingMode", "PTP")
+        p.paragonset("MasterSlave Slave Enabled", "FALSE")
+        p.paragonset("MasterSlave Enabled", "TRUE")
+        p.paragonset("MasterSlave Enabled", "TRUE")
+        p.paragonset("Physical LineInterface", Setup_Conifguration_file.run.paragon_mediaType)
+        p.paragonset("Physical LineRate", Setup_Conifguration_file.run.paragon_speed)
+        if Setup_Conifguration_file.run.paragon_speed == "10GBE":
+            p.paragonset("Physical EthAutonegotiate", "TRUE")
+        elif Setup_Conifguration_file.run.OSA_tested_speed == "100-full":
+            p.paragonset("Physical EthAutonegotiate", "FALSE")
+        else:
+            p.paragonset("Physical EthAutonegotiate", "TRUE")
+        p.paragonset("Physical RefClkSource", "EXT_10M")
+        p.paragonset("Physical OnePpsRefPort", "R75_UNBAL")
+        p.paragonset("MasterSlave Slave Enabled", "FALSE")
+        p.paragonset("MasterSlave StandardsProfile", "G.8275.1_PHASE_PROFILE")
+        p.paragonset("MasterSlave TestConfiguration", "MASTER_TEST")
+        p.paragonset("MasterSlave Slave Enabled", "TRUE")
+        p.paragonset("Capture OnePps WanderCaptEnable", "TRUE")
+        p.paragonset("Capture OnePps AccuracyCaptEnable", "FALSE")
+        p.paragonset("Capture OnePps WanderCaptEnable", "FALSE")
+        teraz2 = time.time()
+        time.sleep(5)
+        p.starttimingcapture()
+        Telnet_OSACalibrationXML.phy_Delays.jump_acc_port("jump")
+        p.stopcapture()
+        time.sleep(3)
+        p.starttimingcapture()
+        d = 0
+        for i in range(Setup_Conifguration_file.run.iterations):
+            d=d+1
+            par.jumps_calibration(d)
+        p.stopcapture()
+        par.save_measurement()
+        print("Whole measurement took exactly:",math.floor(time.time()-teraz2),"seconds.")
+
+    def save_measurement(self):
+        p.paragonset("Cat Show", "TRUE")
+        p.paragonset("Cat", "1588TimeError")
+        p.paragonset("Cat SelectSlot", "2Way")
+        p.paragonset("Cat TIMEERROR ThresholdLimitEnabled", "False")
+        p.paragonset("Cat Autoreload Force", "1")
+        p.paragonset("Cat Autoreload Force", "1")
+        p.paragonset("Cat Calculate", "1")
+        # par=paragon_config.create_path("ścieżki")
+        p.exportdata(ścieżkaCPD)
+        p.paragonset("Cat GenerateReport True", ścieżkaPDF)
+        p.paragonset("Cat", "Close")
+
+    def jumps_calibration(self,d):
+        print("***** ITERATION ***** (",d,"/",Setup_Conifguration_file.run.iterations,")")
+        Telnet_OSACalibrationXML.phy_Delays.jump_acc_port("jump")
 
 
-
-asd=phy_Delays()
-asd.getPhyDelays()
-asd.jump_acc_port()
+par = paragon_config()
